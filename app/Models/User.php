a511c36301851ebcaf26ffa37f84128a44883b92
PerfectionistUA <yaroslav.tsv@gmail.com>
2025-07-05 22:49:47 +0300

    style: fix Pint issues

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,11 +14,12 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     use HasApiTokens;
-    use HasRoles;
 
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
+
     use HasProfilePhoto;
+    use HasRoles;
     use Notifiable;
     use TwoFactorAuthenticatable;
 
